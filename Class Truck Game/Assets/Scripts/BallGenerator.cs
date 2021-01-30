e2e4a6c79824cd2a790113ae823a35473bac7543
Devon <thurgood.devon@gmail.com>
2021-01-30 10:37:59 -0700

    Scripting Practice
    Limited Mud Creation and Created Box destroyer

--- Class Truck Game/Assets/Scripts/BallGenerator.cs
+++ Class Truck Game/Assets/Scripts/BallGenerator.cs
@@ -5,13 +5,15 @@ using UnityEngine;
 public class BallGenerator : MonoBehaviour
 {
     public GameObject ball;
+    int mudCount = 1;
     // Start is called before the first frame update
     IEnumerator Start()
     {
-        while (true)
+        while (mudCount < 10)
         {
             yield return new WaitForSeconds(0.5f);
             Instantiate(ball);
+            mudCount++;
         }
     }
 }
