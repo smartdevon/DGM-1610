63d5b47108ca1b67c5fa0b583f093ae275fa910d
Devon <thurgood.devon@gmail.com>
2021-02-05 10:16:02 -0700

    Fixed User Input; Removed Block Breaker
    Still can't jump for some reason...

--- Class Truck Game/Assets/Scripts/UserInput.cs
+++ Class Truck Game/Assets/Scripts/UserInput.cs
@@ -7,12 +7,14 @@ public class UserInput : MonoBehaviour
     public Rigidbody2D ballRigidbody2D;
     public Vector2 direction;
     public Vector2 yDirection;
+    public float speed = 300;
+    public float jumpForce = 10;
 
 
     // Update is called once per frame
     void Update()
     {
-        direction.x = UserInput.GetAxis("Horizontal") * speed;
+        direction.x = Input.GetAxis("Horizontal") * speed;
         ballRigidbody2D.AddForce(direction, ForceMode2D.Force);
 
         if (Input.GetButtonDown("Jump"))
