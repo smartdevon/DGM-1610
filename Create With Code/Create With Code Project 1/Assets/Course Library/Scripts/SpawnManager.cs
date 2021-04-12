0efd76f6f29b1f7f5d069ef51a438fe34f0284ca
Devon <thurgood.devon@gmail.com>
2021-04-12 17:15:58 -0600

    Spawning Dogs

---
+++ Create With Code/Create With Code Project 1/Assets/Course Library/Scripts/SpawnManager.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject[] animalPrefabs;
+    public int animalIndex;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            Instantiate(animalPrefabs[animalIndex], new Vector3(0, 0, 20), animalPrefabs[animalIndex].transform.rotation);
+        }
+    }
+}
