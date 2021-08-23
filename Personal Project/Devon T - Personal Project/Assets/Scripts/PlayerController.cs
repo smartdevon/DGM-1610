a2ee470262e8d5751faf42daf0faffae268bfffd
Devon <thurgood.devon@gmail.com>
2021-08-23 12:56:16 -0600

    Player Movement
    Implemented player movement. Vertical movement refers to "Up/Down" rather than "North/South". Also, the player ball seems to get confused when it tips over and recognizes other directions as it's proper movement.

--- Personal Project/Devon T - Personal Project/Assets/Scripts/PlayerController.cs
+++ Personal Project/Devon T - Personal Project/Assets/Scripts/PlayerController.cs
@@ -8,6 +8,9 @@ public class PlayerController : MonoBehaviour
     private Rigidbody playerRb;
     public float jumpForce;
     public float gravityModifier;
+    public float horizontalInput;
+    public float verticalInput;
+    public float speed = 10.0f;
 
     // Start is called before the first frame update
     void Start()
@@ -20,11 +23,20 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+        //Horizontal Player Movement
+
+        verticalInput = Input.GetAxis("Vertical");
+        transform.Translate(Vector3.up * verticalInput * Time.deltaTime * speed);
+        //Vertical Player Movement
+
         if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
         {
             playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             isOnGround = false;
         }
+        //Jumping
        
     }
     private void OnCollisionEnter(Collision collision)
